bd804cc6cea55704bb42cc0c739b587c95edd892
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-07-22 11:06:58 +0530

    Updated Solution - GfG to GitHub

--- Easy/Special array reversal/special-array-reversal.cpp
+++ Easy/Special array reversal/special-array-reversal.cpp
@@ -9,29 +9,28 @@ class Solution
     public:
     string reverse(string str)
     { 
-        //code here.
-        stack<char> s;
-        string ans = "";
-        for(int i=0;i<str.length();i++)
+        string ans=str;
+        int i=0;
+        int n = str.length();
+        int j = n-1;
+        while(i<j)
         {
-            if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122))
-            s.push(str[i]);
+            if(str[i]<65 || (str[i]>90 && str[i]<97) || str[i]>122)
+            {
+               i++; 
+            }
+             if(str[j]<65 || (str[j]>90 && str[j]<97) || str[j]>122)
+            {
+               j--;    
+            }
             
+            if(((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122)) && ((str[j]>=65 && str[j]<=90) || (str[j]>=97 && str[j]<=122)))
+            {
+                swap(ans[i],ans[j]);
+                i++;
+                j--;
+            }
             
-        }
-        
-      
-        for(int i=0;i<str.length();i++)
-        {
-         if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122))
-             {
-                 
-                 ans += s.top();
-                 s.pop();
-             }
-             else
-             ans += str[i];
-             
         }
         
         return ans;
