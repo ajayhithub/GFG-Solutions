e8ea3ee0aa07582d99e28b9f086c119d9e2c9346
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2024-03-14 18:57:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Trapping Rain Water/trapping-rain-water.cpp
+++ Medium/Trapping Rain Water/trapping-rain-water.cpp
@@ -12,29 +12,32 @@ class Solution{
     long long trappingWater(int arr[], int n){
         // code here
         
-        long long ans = 0;
-        int pre[n] = {0};
-        int suf[n] = {0};
-        int ma = 0;
-        for(int i=0;i<n;i++)
-        {
-            ma = max(ma,arr[i]);
-            pre[i] = ma;
-        }
-        
-        int m = 0;
-         for(int i=n-1;i>=0;i--)
-        {
-            m = max(m,arr[i]);
-            suf[i] = m;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            ans += min(pre[i],suf[i])-arr[i];
-        }
-        
-        return ans;
+      vector<long long> l(n,0);
+      vector<long long> r(n,0);
+      int ll = 0;
+      for(long long i=0;i<n;i++)
+      {
+          ll = max(ll,arr[i]);
+          l[i] = ll;
+      }
+      
+      int rr = 0;
+       for(long long i=n-1;i>=0;i--)
+      {
+          rr = max(rr,arr[i]);
+          r[i] = rr;
+      }
+      
+      long long ans = 0;
+      
+      for(long long i=0;i<n;i++)
+      {
+         if(min(l[i],r[i])-arr[i] > 0)
+         ans += min(l[i],r[i])-arr[i];  
+         
+      }
+      
+       return ans;    
         
     }
 };
